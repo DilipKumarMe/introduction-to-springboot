79c1b00658cd16b4bfd4ef50fd125ab1afc3fb8c
Dilip Kumar <dilipkumarme1620@gmail.com>
2024-11-10 15:07:00 +0530

    adding learn springboot project

---
+++ src/main/java/com/dilipcode/dilip/springbootIntro/introduction/to/springboot/IntroductionToSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dilipcode.dilip.springbootIntro.introduction.to.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dilipcode/dilip/springbootIntro/introduction/to/springboot/IntroductionToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilipcode.dilip.springbootIntro.introduction.to.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
